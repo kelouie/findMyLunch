a0f856195eabe74905fb9397dafd8cf7cbc833e9
Katharine Louie <KatharineLouie@carfax.com>
2016-10-20 11:44:29 -0400

    Removed spring boot jpa stuff (no need to connect to db)

--- src/main/groovy/com/carfax/food/domain/FoodRequest.groovy
+++ src/main/groovy/com/carfax/food/domain/FoodRequest.groovy
@@ -2,14 +2,13 @@ package com.carfax.food.domain
 
 import groovy.transform.Canonical
 
-import javax.persistence.Entity
+
 
 
 /**
  * Created by katharinelouie on 6/10/16.
  */
 @Canonical
-@Entity
 class FoodRequest {
 
 }
