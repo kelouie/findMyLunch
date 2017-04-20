b2ef0cbcb9305f047ba2056719c6c447ee234ca8
Katharine Louie <KatharineLouie@carfax.com>
2017-04-20 16:27:35 -0400

    Mapped the conference rooms and food types

---
+++ src/main/groovy/com/carfax/food/domain/FoodRequestTypes.groovy
@@ -0,0 +1,15 @@
+package com.carfax.food.domain
+
+class FoodRequestTypes {
+
+    Map foodTypeMapping = [
+            2 : 'Breakfast - Cold - 48 hour notice required',
+            9 : 'Breakfast - Hot - 72 hour notice required',
+            5 : 'Deli Platter - 72 hour notice required',
+            1 : 'Drinks ONLY - 24 hours notice required',
+            8 : 'Lunch Box - 48 hour notice required',
+            7 : 'Pizza - 24 hour notice required',
+            3 : 'Popcorn Machine - 24 hours notice required',
+            6 : 'Snacks - 24 hours  notice required',
+            4 : 'Special Event - 72 hours notice required'
+}
